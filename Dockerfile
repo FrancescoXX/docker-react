55515763dbb01c6d8fe1a1de24f1a027f26b8d93
FrancescoXX <francescociullaroma@gmail.com>
2021-01-21 14:16:56 +0100

    added tailwindCSS Support, fix docker compose, 5 buttons to test crud api

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,24 @@
 # 1 Build Dependencies
 FROM node:14 as dependecies
+
 WORKDIR /usr/src/app
-COPY package.json yarn.lock ./
-RUN yarn
+
+COPY package.json package-lock.json ./
+
+# Use latest version of npm
+RUN npm install npm@latest -g
+
+RUN npm install --no-optional && npm cache clean --force
+
 COPY . ./
-RUN yarn build
+
+RUN npm run build
 
 # 2 Nginx
 FROM nginx:1.12-alpine
+
 COPY --from=dependecies /usr/src/app/build /usr/share/nginx/html
-EXPOSE 80
+
+EXPOSE 8080 
+
 CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="flex-shrink-0">
+          <img class="h-12 w-12" src={logo} alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div class="text-xl font-medium text-black">Tailwind Integration</div>
+          <p class="text-gray-500">Done!</p>
+        </div>
+        
+      </div>
+      <div>
+        <button class="m-2 py-6 px-6 font-semibold rounded-lg shadow-md text-white text-xl bg-green-700 hover:bg-green-900">Get All</button>
+      </div>
+      <div>
+        <button class="m-2 py-6 px-6 font-semibold rounded-lg shadow-md text-white text-xl bg-green-500 hover:bg-green-700">Get One</button>
+      </div>
+      <div>
+        <button class="m-2 py-6 px-6 font-semibold rounded-lg shadow-md text-white text-xl bg-yellow-500 hover:bg-yellow-700">Create One</button>
+      </div>
+      <div>
+        <button class="m-2 py-6 px-6 font-semibold rounded-lg shadow-md text-white text-xl bg-purple-500 hover:bg-purple-700">Update One</button>
+      </div>
+      <div>
+        <button class="m-2 py-6 px-6 font-semibold rounded-lg shadow-md text-white text-xl bg-red-500 hover:bg-green-700">Delete one</button>
+      </div>
     </div>
   );
 }
